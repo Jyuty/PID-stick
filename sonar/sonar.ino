2519a8bb0092dc8dc496e7e6d0cd21261c68cf81
GlebSr <g4946532@gmail.com>
2021-02-26 01:09:36 +0300

    Запустил сонар и вывел его показания в мм

--- sonar/sonar.ino
+++ sonar/sonar.ino
@@ -1,9 +1,25 @@
+#define sonarTrigPIN 2
+#define sonarEchoPIN 3
+
+unsigned long duration;
+float dist;
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(sonarTrigPIN,OUTPUT);
+  pinMode(sonarEchoPIN,INPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(sonarTrigPIN, LOW); 
+  delayMicroseconds(2); 
+  digitalWrite(sonarTrigPIN, HIGH); 
+  delayMicroseconds(10); 
+  digitalWrite(sonarTrigPIN, LOW); 
+  duration = pulseIn(sonarEchoPIN,HIGH);
+  dist = (float)duration / 5.75;
+  //выводит в мм
+  Serial.println(dist);
+  delay(100);
+  
 }
